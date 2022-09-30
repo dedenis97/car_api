14ac5349a66292af474ec73b78baa60c3039d8a5
dedenis97 <dvarotto97@gmail.com>
2022-09-30 15:15:40 +0200

    move typeorm config to databasemodule and import in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,53 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './users/entity/user.entity';
-import { ReportEntity } from './reports/entity/report.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 const cookieSession = require('cookie-session')
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			envFilePath: "env/.env." + process.env.NODE_ENV,
-		}),
-		TypeOrmModule.forRootAsync({
-			inject: [ConfigService],
-
-			useFactory: (config: ConfigService) => {
-				return {
-					type: "mysql",
-					host: '127.0.0.1',
-					port: 3308,
-					username: 'root',
-					password: 'helloworld',
-					database: config.get("DB_NAME"),
-					entities: [UserEntity, ReportEntity],
-					
-					// map the entity to the database
-					// is is 'true'
-
-					// NOT USE this in production!!!!!!
-					// use ONLY in development!!!!!
-
-
-					synchronize: true,
-
-					// Dangerous :
-
-					// if i remove a prop in an entity
-					// typeorm will remove the column and the data inside
-					// from the entity table
-					// so i will lost all the data from the table in that column
-
-				}
-			}
-
-		}),
-
+		DatabaseModule,
 		UsersModule,
 		ReportsModule,
 	],
---
+++ src/database/database.module.ts
@@ -0,0 +1,53 @@
+import { Module } from "@nestjs/common"
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { ReportEntity } from "src/reports/entity/report.entity"
+import { UserEntity } from "src/users/entity/user.entity"
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: "src/env/.env." + process.env.NODE_ENV,
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+
+            useFactory: (config: ConfigService) => {
+                return {
+                    type: "mysql",
+                    host: config.get<string>("DB_HOST"),
+                    port: config.get("DB_PORT"),
+                    username: config.get<string>("DB_USERNAME"),
+                    password: config.get<string>("DB_PASSWORD"),
+                    database: config.get<string>("DB_NAME"),
+                    entities: [UserEntity, ReportEntity],
+
+                    // map the entity to the database
+                    // is is 'true'
+
+                    // NOT USE this in production!!!!!!
+                    // use ONLY in development!!!!!
+
+
+                    synchronize: true,
+
+                    // Dangerous :
+
+                    // if i remove a prop in an entity
+                    // typeorm will remove the column and the data inside
+                    // from the entity table
+                    // so i will lost all the data from the table in that column
+
+                }
+            }
+
+        }),
+
+
+    ],
+
+})
+
+
+export class DatabaseModule { }
